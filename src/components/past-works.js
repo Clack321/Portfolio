da957cf9cc27b4085c60c1b231354910eca49549
David Johnson <dajohnson1998@gmail.com>
2019-04-15 14:15:38 -0400

    styling for past works is pretty much done

--- src/components/past-works.js
+++ src/components/past-works.js
@@ -9,11 +9,18 @@ class PastWorks extends Component {
   render() {
     return (
       <div id="App">
+      {console.log(this.props.PastWorks)}
         <SideBar pageWrapId={"inner-box"} outerContainerId={"App"} />
         <main className="outer-box">
           <div id="inner-box">
-          <p>
-          </p>
+            <h1 className="past-works-title">{this.props.pastWorks[0].title}</h1>
+            <p className="past-works-description">{this.props.pastWorks[0].description}</p>
+            <div className="button-container">
+              <button className="past-works-button" onClick={e => console.log("Cool Features Clicked!")}>Cool Features</button>
+              <button className="past-works-button" onClick={e => console.log("Tech Stack Clicked!")}>Tech Stack</button>
+              <button className="past-works-button" onClick={e => console.log("Extra Info Clicked!")}>Extra Info</button>
+            </div>
+            <article className="past-works-output-box"></article>
           </div>
         </main>
         <Footer />
@@ -23,7 +30,7 @@ class PastWorks extends Component {
 }
 
 const mapStateToProps = state => ({
-
+pastWorks : state.reducer.projects
 });
 
 export default connect(mapStateToProps)(PastWorks);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,13 +4,13 @@ import {
 
 const initialState = {
     projects : [
-      {title: "Learn Venture",
+      {title: "Learn Spanish!",
       description: "A language learning app that uses a spaced repitition algorithm to help you native English speakers learn Spanish!",
       appPhotoLink: "...",
       coolFeatures: ["Learn Spanish keeps track of every right or wrong answer you make allowing you to view your progress!",
                       "We also have a chatroom where anyone (logged in) can play! The first person the guess the correct english traslation wins!"],
       techStack: ["Node.Js", "MongoDB", "Mongoose", "Bcrypt", "Passport", "socketIO", "Axios", "React"],
-      extraInfo: "",
+      extraInfo: "Me and a Friend created this project, you can check him out on linkIn at: ",
       gitHubLink: "https://github.com/thinkful-ei27/david-john-spaced-rep"
       }
     ]
