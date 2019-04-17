a6a19cd4f1f8f1929a3210d7fa3ffdbabae0ba14
David Johnson <dajohnson1998@gmail.com>
2019-04-16 22:41:57 -0400

    updated about-me page

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -8,9 +8,9 @@ export function AboutMe(props) {
   return (
     <main className="about-me">  
     <div className="about-me-text-box">
-      <p>I've worked hard all my life, Whether that meant working in a warehouse dragging 100 pound cabinets around at 14 years old,
+      <p>I've worked hard all my life. Whether that meant working in a warehouse dragging 100 pound cabinets around at 14 years old,
         making money in my vast array of side hustles or even working a perpetual 60 hour a week in construction.
-        after a year out of highschool i felt like I school was the best road forward,
+        However, that quickly began to get old, so about a year and a half after highschool i felt like going back to school was the best road forward,
         so I went on to community college and took all of the hardest engineering classes I could in order to prove to myself that I was smart enough to make it in the world.
         After a few semesters I took a java programming class, and immediately I was hooked, the next semester I took a class in C++ and I knew for sure, that this was what I wanted to do with my life.
         I had so much fun learning I finished the whole class's curriculum in a few weeks.
